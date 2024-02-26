d93d8fa6149ae2a9242375fe0ce18e058579b4e2
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-02-26 00:41:12 -0300

    refactor: move user logic to service

--- backend/src/main/java/com/db/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/db/backend/controller/AuthenticationController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,7 +22,7 @@ import com.db.backend.dto.UserRegistrationRequestDTO;
 import com.db.backend.dto.UserRegistrationResponseDTO;
 import com.db.backend.entity.User;
 import com.db.backend.infra.security.JwtService;
-import com.db.backend.repository.UserRepository;
+import com.db.backend.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -39,7 +38,7 @@ public class AuthenticationController {
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private UserRepository repository;
+    private UserService userService;
 
     @Autowired
     private JwtService jwtService;
@@ -65,12 +64,10 @@ public class AuthenticationController {
     })
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody @Valid UserRegistrationRequestDTO data) {
-        if (this.repository.findByEmail(data.email()) != null) {
+        if (this.userService.existsByEmail(data.email())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("E-mail already in use");
         }
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.name(), data.email(), encryptedPassword);
-        this.repository.save(newUser);
+        this.userService.registerNewUser(data);
         return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
     }
 
@@ -78,7 +75,7 @@ public class AuthenticationController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
--- backend/src/main/java/com/db/backend/dto/UserRegistrationRequestDTO.java
+++ backend/src/main/java/com/db/backend/dto/UserRegistrationRequestDTO.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRegistrationRequestDTO(
-    @NotBlank(message = "Name is mandatory") @FullName String name,
-    @NotBlank(message = "Email is mandatory") @Email(message = "User must provide a valid e-mail") String email,
-    @Size(min = 8, max = 12, message = "Password must be between 8 and 12 characters") String password) {
+        @NotBlank(message = "Name is mandatory") @FullName String name,
+        @NotBlank(message = "Email is mandatory") @Email(message = "User must provide a valid e-mail") String email,
+        @Size(min = 8, max = 12, message = "Password must be between 8 and 12 characters") String password) {
 }
