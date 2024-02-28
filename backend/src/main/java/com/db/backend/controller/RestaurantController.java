1132bde450b32b9acaebc7acc0a59b7b1b2cc032
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-02-28 13:45:52 -0300

    feat: create controller method for registering a new restaurant

--- backend/src/main/java/com/db/backend/controller/RestaurantController.java
+++ backend/src/main/java/com/db/backend/controller/RestaurantController.java
@@ -1,15 +1,37 @@
 package com.db.backend.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.db.backend.dto.RestaurantDTO;
+import com.db.backend.service.RestaurantService;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("restaurant")
 @Tag(name = "Restaurant")
 public class RestaurantController {
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping()
+    public ResponseEntity<Long> registerRestaurant(@RequestBody @Valid RestaurantDTO restaurantDTO) {
+        Long restaurantId = restaurantService.registerNewRestaurant(restaurantDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(restaurantId);
+    }
+
     @GetMapping()
     public String listRestaurant() {
         return "List of restaurants";
