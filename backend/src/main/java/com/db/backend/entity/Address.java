2c097b2df4f94adb66242f4e37f89de504960fc8
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-02-28 13:01:34 -0300

    feat: create Restaurant and Address classes

---
+++ backend/src/main/java/com/db/backend/entity/Address.java
@@ -0,0 +1,67 @@
+package com.db.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "addresses")
+@Entity(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Number is mandatory")
+  private String number;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Postal Code is mandatory")
+  @Size(min = 9, message = "User must provide a valid postal code")
+  private String postCode;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Neighborhood is mandatory")
+  private String neighborhood;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Street is mandatory")
+  private String street;
+
+  @Column(nullable = false)
+  @NotBlank(message = "City is mandatory")
+  private String city;
+
+  @Column(nullable = false)
+  @NotBlank(message = "State is mandatory")
+  private String state;
+
+  @OneToOne(mappedBy = "address")
+  private Restaurant restaurant;
+
+  public Address(String number, String postCode, String neighborhood, String street, String city, String state) {
+    this.number = number;
+    this.postCode = postCode;
+    this.neighborhood = neighborhood;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+  }
+
+}
---
+++ backend/src/main/java/com/db/backend/entity/Restaurant.java
@@ -0,0 +1,65 @@
+package com.db.backend.entity;
+
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "restaurants")
+@Entity(name = "restaurants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Restaurant {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Name is mandatory")
+  private String name;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Description is mandatory")
+  private String description;
+
+  @Column
+  private String website;
+
+  @Column
+  private String image;
+
+  @Column(nullable = false)
+  private Boolean winnerBlock = false;
+
+  @OneToOne
+  @JoinColumn(name = "address_id")
+  @RestResource(path = "restaurantAddress", rel = "address")
+  @NotNull(message = "Restaurant is mandatory")
+  private Address address;
+
+  public Restaurant(String name, String description, String website, String image, Address address) {
+    this.name = name;
+    this.description = description;
+    this.website = website;
+    this.image = image;
+    this.winnerBlock = false;
+    this.address = address;
+  }
+
+}
