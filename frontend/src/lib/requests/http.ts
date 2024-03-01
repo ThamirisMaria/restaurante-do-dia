8fc8547f3920cbc5f469eb1108f101daaca4c6d9
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-01 15:02:05 -0300

    feat: create global request headers
    Co-authored-by: JuliaSCHLima <JuliaSCHLima@users.noreply.github.com>

--- frontend/src/lib/requests/http.ts
+++ frontend/src/lib/requests/http.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
 
+interface HeadersProps {
+  Authorization?: string;
+}
+
+const accessToken = localStorage.getItem("restaurant:access_token");
+
+const headers: HeadersProps = {};
+
+if (accessToken) {
+  headers.Authorization = `Bearer ${accessToken}`;
+}
+
 export const http = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: { ...headers },
 });
