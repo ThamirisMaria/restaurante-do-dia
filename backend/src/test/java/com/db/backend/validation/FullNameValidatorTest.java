52b530049583b29d0c5179b6a8362602dc2173dd
Thamiris Maria <73439911+ThamirisMaria@users.noreply.github.com>
2024-03-07 11:59:39 -0300

    Update backend/src/test/java/com/db/backend/validation/FullNameValidatorTest.java

--- backend/src/test/java/com/db/backend/validation/FullNameValidatorTest.java
+++ backend/src/test/java/com/db/backend/validation/FullNameValidatorTest.java
@@ -8,26 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FullNameValidatorTest {
     @Test
-    public void test1() {
-       var result = new FullNameValidator (). isValid("julia", null);
+    public void fullNameValidator_should_not_accept_single_name() {
+       var result = new FullNameValidator().isValid("julia", null);
 
         assertEquals (false, result);
     }
 
     @Test
-    public void test2() {
-       var result = new FullNameValidator (). isValid("julia lima", null);
+    public void fullNameValidator_should_accept_two_word_names() {
+       var result = new FullNameValidator().isValid("julia lima", null);
 
         assertEquals (true, result);
-
     }
 
     @Test
-    public void test3() {
-       var result = new FullNameValidator (). isValid("julia p", null);
+    public void fullNameValidator_should_not_accept_one_letter_names() {
+       var result = new FullNameValidator().isValid("julia p", null);
 
         assertEquals (false, result);
-
-
     }
 }
